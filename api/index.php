a7c54763714467aed31523401b37e617adef6e5f
sarslanamjad-crypto <sarslanamjad-crypto@users.noreply.github.com>
2026-06-20 23:53:09 +0500

    Configure Vercel cached files path to /tmp

--- api/index.php
+++ api/index.php
@@ -1,1 +1,7 @@
-<?php require __DIR__ . '/../public/index.php';
+<?php
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+require __DIR__ . '/../public/index.php';
