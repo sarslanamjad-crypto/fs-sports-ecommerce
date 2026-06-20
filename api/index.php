36e6ca91b1c2dbed0317c22e8ea610fbc0690a50
sarslanamjad-crypto <sarslanamjad-crypto@users.noreply.github.com>
2026-06-20 23:56:52 +0500

    Use putenv for Vercel cache paths configuration

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
-$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
-$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
-$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
-$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
 require __DIR__ . '/../public/index.php';
